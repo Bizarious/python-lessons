63fa57d71f7e51446b06bb1d0949b789f27ee6e3
bizarious <bizardev@web.de>
2021-10-20 21:20:45 +0200

    fixed some missing information

--- latex/slides/resources/01_getting_started/string_format.py
+++ latex/slides/resources/01_getting_started/string_format.py
@@ -1,7 +1,7 @@
-# mit `str.format()`  
+# mit `str.format()`
 
 'my string {} {}'.format(4, 'vier')
-# in Reihenfolge der argumente
+# in Reihenfolge der Argumente
 
 'my string {number} {name}'.format(name='vier', number=4)`
 # via Name, Reihenfolge egal
@@ -11,10 +11,10 @@
 
 # und mit dem %-Operator
 
-'string %d %s' % (4, 'vier')
+'my string %d %s' % (4, 'vier')
 # in Reihenfolge
 
-'string %(number)d %(name)s' % {number:4, name:'vier'}
+'my string %(number)d %(name)s' % {'number':4, 'name':'vier'}
 # via Name
 
 # mit f strings
