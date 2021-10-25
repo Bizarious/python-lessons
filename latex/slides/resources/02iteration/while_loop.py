fcda9a1a488d6761693e5371e7f7ae254b057c59
bizarious <bizardev@web.de>
2021-10-25 16:54:19 +0200

    added while loop python file

---
+++ latex/slides/resources/02iteration/while_loop.py
@@ -0,0 +1,17 @@
+while bedingung:
+    print("Die Bedingung ist wahr")
+
+number = 0
+
+# die Bedingung wird erst False, wenn number größer oder gleich 5 ist
+while number < 5:
+    print(number)
+    number += 1
+
+while True:
+    print("Endlosschleife")
+
+while True:
+    # es wird nur einmal "Endlosschleife" ausgegeben
+    print("Endlosschleife")
+    break
