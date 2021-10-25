c49f61386e2a1f67d957b6517cb46fc9c0986cac
bizarious <bizardev@web.de>
2021-10-25 16:54:04 +0200

    added numbers python file

---
+++ latex/slides/resources/02iteration/numbers.py
@@ -0,0 +1,27 @@
+number = 42
+number = -42
+
+number = 42.1337
+number = -42.1337
+
+number = 5 + 3 # Addition
+number = 5 - 3 # Subtraktion
+number = 5 * 3 # Multiplikation
+number = 5 / 3 # Division
+number = 5 % 3 # Modulo
+
+number = 5
+number = number + 3
+# das '+' mit der gewünschten Operation ersetzen
+
+# dieser Code tut das gleiche wie oben
+number = 5
+number += 3
+# das '+' mit der gewünschten Operation ersetzen
+
+5 == 5 # gleich
+5 != 3 # ungleich
+5 < 6 # kleiner
+5 <= 6 # kleiner gleich
+5 > 3 # größer
+5 >= 3 # größer gleich
