09b08f86b1ce28dc3771d334e56e6f9643b884f8
Pollopiccante <richard_mueller4@aol.de>
2021-10-27 14:30:34 +0200

    list anpassungen

--- latex/slides/resources/02iteration/list.py
+++ latex/slides/resources/02iteration/list.py
@@ -1,13 +1,16 @@
 # eine Liste initialisieren
 prime_list = [2, 3, 5, 7, 11, 13]
 
-# Listen können jegliche Objekte, auch gemischt enthalten
-stuff = [5, 1.56363, False, prime_list, None]
+# Listen können jegliche Objekte,
+# auch gemischt und mehrfach enthalten
+stuff = [5, 1.56363, 5, False, prime_list, None, False]
 
 # einzelne Elemente referenzieren
 first_prime = prime_list[0] #2
 third_prime = prime_list[2] #5
 last_prime = prime_list[-1] #13
+# Elemente zu schreiben ist auch möglich
+prime_list[0] = 42
 
 # neue Elemente anfügen
 prime_list.append(15)
@@ -16,3 +19,7 @@ print(prime_list) #[2, 3, 5, 7, 11, 13, 15]
 # Elemente entfernen
 prime_list.remove(11)
 print(prime_list) #[2, 3, 5, 7, 13, 15]
+
+# Elemente per Index entfernen
+last_prime = prime_list.pop() #13
+print(prime_list) #[2, 3, 5, 7, 11]
