8ec4f463d957291a87f8cc77bb8b80db5c063946
bizarious <bizardev@web.de>
2021-11-02 17:41:36 +0100

    added further slides for functions

--- latex/slides/resources/03bool_fun_dict/func.py
+++ latex/slides/resources/03bool_fun_dict/func.py
@@ -3,3 +3,16 @@ def my_funtion():
 
 # irgendwo im Programmablauf
 my_function()
+
+# beliebig viele Argumente mit Komma getrennt in die Klammern
+def potenz(x, y):
+    print(x**y)
+
+# Argumente werden in der Reihenfolge belegt, wie sie angegeben wurden
+potenz(2, 3) # x = 2, y = 3
+
+def potenz(x, y):
+    # x^y wird zurückgegeben
+    return x**y
+
+p = potenz(2, 3) # p = 8
