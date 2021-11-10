544b597fad14a3184ca7f17b00f545fbb66febee
Pollopiccante <richard_mueller4@aol.de>
2021-11-10 13:10:58 +0100

    added list to tuple explanation

--- latex/slides/resources/04tuples/tuples.py
+++ latex/slides/resources/04tuples/tuples.py
@@ -1,5 +1,5 @@
 # Initialisierung mit ()
-tuple_eins = (1, True, 4.5, "hallo")
+tupel_eins = (1, True, 4.5, "hallo")
 
 # Elemente abfragen wie bei Listen
 viereinhalb = tuple_eins[2] #4.5
@@ -7,13 +7,17 @@ hallo = tuple_eins[-1] #'hello'
 vordere = tuple_eins[:2] #(1, True)
 
 # Elemente verändern, unmöglich !
-tuple_eins[0] = "neuer Wert" #TypeError
+tupel_eins[0] = "neuer Wert" #TypeError
 # Ganzes Tuple austauschen, möglich !
-tuple_eins = ("neuer Wert", True, 4.5, "hallo")
+tupel_eins = ("neuer Wert", True, 4.5, "hallo")
 
 # mehrere Werte gleichzeitig abfragen
-positions_tuple = (45.65, 198.12)
+positions_tupel = (45.65, 198.12)
 (x, y) = positions_tuple # x: 45.65, y: 198.12
 
 # unter anderem nützlich für startwerte
 (x, y, z) = (0, 100, 200) #x: 0, y: 100, z:200
+
+# Liste zu Tupel umwandeln
+liste = [1,2,3]
+tupel_aus_liste = tuple(liste)
