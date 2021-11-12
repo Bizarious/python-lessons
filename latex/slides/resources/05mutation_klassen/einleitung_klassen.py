1cc6e3726337e2298d839e834740f06bccf2fc79
Pollopiccante <richard_mueller4@aol.de>
2021-11-12 19:22:19 +0100

    classes introduction and constructor

---
+++ latex/slides/resources/05mutation_klassen/einleitung_klassen.py
@@ -0,0 +1,14 @@
+class Rennwagen:
+    def __init__(self):
+        self.speed = "sehr hoch"
+
+rennwagen1 = Rennwagen()
+class Kuh:
+    def muh(self):
+        print("MUHH")
+
+kuh1 = Kuh()
+kuh2 = Kuh()
+
+rennwagen1.speed
+kuh1.muh()
