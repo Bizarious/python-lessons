634191a0b79bd861228155f65cea99b6bb5115f2
Pollopiccante <richard_mueller4@aol.de>
2021-11-24 16:05:21 +0100

    exercise 2 update

--- exercise/exercise_06/class_list.py
+++ exercise/exercise_06/class_list.py
@@ -5,5 +5,5 @@ fahrzeuge = [Fahrzeug(1000, 10), Fahrzeug(20000, 100)]
 
 for fahrzeug in fahrzeuge:
     print(fahrzeug.info())
-    fahrzeug.fahren(100)
     fahrzeug.hupen()
+    fahrzeug.fahren(100)
