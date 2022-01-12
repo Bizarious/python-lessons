603f3650b5d9661dff9a4cb331e3ce7d19b78596
bizarious <bizardev@web.de>
2022-01-12 14:43:19 +0100

    added exercise 9

---
+++ exercise/exercise_09/comprehensions.py
@@ -0,0 +1,16 @@
+def first():
+    with open("test.txt", "r+") as f:
+        s = f.read()
+        s_new = "".join(["abc" if i == "a" else "bb" for i in s])
+        f.seek(0)
+        f.write(s_new)
+
+
+def second():
+    with open("test.txt", "r+") as f:
+        d = {"a": "abc", "b": "bb", "c": "cac"}
+        s = f.read()
+        s_new = "".join([d[i] for i in s])
+        f.seek(0)
+        f.write(s_new)
+
